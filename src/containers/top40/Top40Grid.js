b1fbf2fcfc3ca6b08d4e2b6f9649820d8e951a3f
onitwin <onitwin@gmail.com>
2021-03-31 14:52:28 +0100

    correct layout on page 1 achieved

--- src/containers/top40/Top40Grid.js
+++ src/containers/top40/Top40Grid.js
@@ -7,14 +7,14 @@ const Top40Grid=()=>{
   <div className="top-40-banner-wrap">
   <img className="banner-image" src="./top-40-banner.webp"/>
   </div>
-  <img className="top-40-brand" src="./top-20-logos-bahco-1.webp"/>
+  <img className="top-40-brand" src="./top-20-logos-makita-1.webp"/>
+  <img className="top-40-brand" src="./top-20-logos-dewalt-1.webp"/>
+  <img className="top-40-brand" src="./top-20-logos-hikoki-1.webp"/>
   <img className="top-40-brand" src="./top-20-logos-bosch-1.webp"/>
   <img className="top-40-brand" src="./top-20-logos-bahco-1.webp"/>
-  <img className="top-40-brand" src="./top-20-logos-bosch-1.webp"/>
-  <img className="top-40-brand" src="./top-20-logos-bahco-1.webp"/>
-  <img className="top-40-brand" src="./top-20-logos-bosch-1.webp"/>
-  <img className="top-40-brand" src="./top-20-logos-bahco-1.webp"/>
-  <img className="top-40-brand" src="./top-20-logos-bosch-1.webp"/>
+  <img className="top-40-brand" src="./top-20-logos-metabo-1.webp"/>
+  <img className="top-40-brand" src="./top-20-logos-stanley-1.webp"/>
+  <img className="top-40-brand" src="./top-20-logos-evolution-1.webp"/>
 
   </div>
 )
