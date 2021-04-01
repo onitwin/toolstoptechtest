d65d686e7d5836744691fb1f1da0549dc38a97dc
onitwin <onitwin@gmail.com>
2021-04-01 09:46:44 +0100

    basic fetch working in background

--- src/containers/top40/Top40Branded.js
+++ src/containers/top40/Top40Branded.js
@@ -3,6 +3,22 @@ import React,{useState,useEffect} from 'react'
 
 const Top40Branded=({brand})=>{
 
+  const [brands,setBrands]=useState([]);
+
+
+    function fetchfunction(brand){
+    const url=`https://product-fetch-toolstop.herokuapp.com/top40/${brand}`;
+    fetch(url)
+    .then(data=>data.json())
+    .then((data)=>setBrands(data))
+  }
+
+  useEffect(()=>{
+    fetchfunction(brand)
+  })
+
+
+
   return(
     <div>
     The passed in brand for the fetch is {brand}
