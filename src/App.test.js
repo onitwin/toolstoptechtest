9c22d84925197bdde72374dfee6ecb98e76f713a
onitwin <onitwin@gmail.com>
2021-04-02 17:38:57 +0100

    tried initial tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import MainContainer from "./containers/maincontainer/MainContainer"
 
 // test('renders learn react link', () => {
 //   render(<App />);
 //   const linkElement = screen.getByText(/learn react/i);
 //   expect(linkElement).toBeInTheDocument();
 // });
+
+
+test('renders top 40 text',()=>{
+  render(<MainContainer/>);
+  const textElement=screen.getByText(/Top 40/i);
+  expect(textElement).toBeInTheDocument();
+})
