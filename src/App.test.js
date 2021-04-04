533fc5e2b03597c052fa1cc65fbc4348255f5b6d
onitwin <onitwin@gmail.com>
2021-04-04 20:19:04 +0100

    added error test

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import MainContainer from "./containers/maincontainer/MainContainer"
+import ErrorPage from "./containers/errors/ErrorPage"
 
 // test('renders learn react link', () => {
 //   render(<App />);
@@ -12,5 +13,12 @@ import MainContainer from "./containers/maincontainer/MainContainer"
 test('renders top 40 text',()=>{
   render(<MainContainer/>);
   const textElement=screen.getByText(/Top 40/i);
+  expect(textElement).toBeInTheDocument();
+})
+
+
+test('renders error message',()=>{
+  render(<ErrorPage/>);
+  const textElement=screen.getByText(/This result has returned an error/i);
   expect(textElement).toBeInTheDocument();
 })
